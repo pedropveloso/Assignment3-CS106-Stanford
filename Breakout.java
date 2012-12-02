96efcc7902256d53c0dbb0dc2cb086ef10324eed
S <s>
2012-12-02 01:30:23 +0000

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -66,7 +66,7 @@ public class Breakout extends GraphicsProgram {
 	private static final int NTURNS = 3;
 	
 /** Animation cycle delay */
-	private static final int DELAY = 2;	
+	private static final int DELAY = 5;	
 
 /* Method: run() */
 /** Runs the Breakout program. */
@@ -149,7 +149,7 @@ public class Breakout extends GraphicsProgram {
 			ball.sendToBack();
 			vx = rgen.nextDouble(1.0, 3.0);
 			if (rgen.nextBoolean(0.5)) vx = -vx;
-			vy = 5;
+			vy = 3;
 		}
 	}
 	
